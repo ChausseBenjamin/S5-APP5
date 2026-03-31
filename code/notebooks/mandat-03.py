dab1ba2197a72f563228be068ae93e35f8eada0e
LyamBRS <cous5830@gmail.com>
2026-03-31 16:23:50 +0000

    Text du mandat-03.

--- code/notebooks/mandat-03.py
+++ code/notebooks/mandat-03.py
@@ -22,10 +22,145 @@ with app.setup:
     import marimo as mo
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Mandat no. 3 : Simulations Monte-Carlo
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Fonctions des temps d'arrivés
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### Processus de poisson et exponentielle.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    La fonction de masse de la loi de Poisson est donnée par :
+    $$
+    P(X=x)=\frac{e^{-\lambda T}(\lambda T)^x}{x!}
+    $$
+    - Où $\lambda$ est le taux d'arrivée.
+    - T est une durée de temps.
+
+
+    Cette fonction donne la probabilité que $x$ arrivées surviennent dans un intervalle de temps $T$. Cependant, ici on s'intéresse plutôt au *temps entre chaque arrivée*.
+    Si $x$ représente le nombre d'arrivées dans l'intervalle $[0,T]$, posé $x=0$ devrait donnée la probabilité qu'aucune arrivée ne survienne dans cet intervalle.
+
+    Autrement dit, l'absence d'événement dans $[0,T]$ c'est la probabilité que le temps jusqu'à la prochaine arrivée soit plus grand que $T$:
+    $$
+    P(T_A > T)
+    $$
+
+    $x$ est donc remplacé par $0$ dans la formule de Poisson, et c'est simplifié:
+    $$
+    P(X=0)=\frac{e^{-\lambda T}(\lambda T)^0}{0!}
+    $$
+    $$
+    P(X=0)=\frac{e^{-\lambda T}\cdot 1}{1}
+    $$
+    $$
+    P(X=0)=e^{-\lambda T}
+    $$
+
+    Ça donne une fonction exponentielle décroissante. Plus $T$ augmente, plus la probabilité diminue.
+    C'est la fonction de survie:
+    $$
+    P(T_A > T) = e^{-\lambda T}
+    $$
+
+    La CDF s'obtient en prenant le complément, étant donnée la présence d'un $>$ dans la formule de survie.
+    $$
+    F(T)=P(T_A \le T)=1 - e^{-\lambda T}
+    $$
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### Inversion de la fonction
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Chaque joueur arrive à un temps $P$, puis quitte après une session de jeu de durée aléatoire $Q$. Donc, chaque joueur est actif dans un intervalle $[P, P+Q]$.
+
+    L'activité d'un joueur est donc:
+    $$
+    P \le t \le P + Q
+    $$
+
+    Ça permet de déterminer combien de joueurs sont actifs à n'importe quel instant (minute) $t$.
+    La génération des temps d'arrivée est fait avec la méthode de transformation inverse.
+    Il faut donc appliqué une transformation inverse sur la CDF trouvé plus haut. On résoud pour $T$.
+    $$
+    F(T)=1 - e^{-\lambda T}
+    $$
+    $$
+    U = 1 - e^{-\lambda T}
+    $$
+    $$
+    U - a = e^{-\lambda T}
+    $$
+    $$
+    \ln(1 - U) = -\lambda T
+    $$
+    $$
+    T = \frac{\ln(1 - U)}{-\lambda}
+    $$
+    Ici, $U$ est une variable aléatoire continue uniforme entre $[0,1]$.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### Python
+    La fonction python suivante génère les temps d'arrivés $P$ en utilisant la transformation inverse trouvé plus haut.
+    Elle prend comme paramètres le taux d'arrivés par minutes et le nombre de données à générés.
+    """)
+    return
+
+
 @app.function
 def generate_arrival_times(arrivals_per_minutes, sample_size):
     uniform_randoms = np.random.rand(sample_size)
     return -np.log(uniform_randoms) / arrivals_per_minutes
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Temps de jeux des joueurs
+    La variable aléatoire normale $Q$ a été déterminée dans le mandat 2, avec sa moyenne et sa variance.
+    Elle dicte le nombre d'heures qu'un joueur passe sur le jeux.
+
+    La variable est indépendante du temps d'arrivé ou tout autres valeurs associés au mandat-03.
+
+    Pour la générer l'implémentation manuel de l'algorithme de Box-Muller est fesable, mais c'est inutile,
+    puisque NumPy a déjà une implémentation dans la fonction `randn`, qui à été confirmé comme équivalente
+    lors des laboratoires.
+    """)
+    return
 
 
 @app.function
@@ -33,13 +168,39 @@ def generate_play_durations(average, variance, sample_size):
     return np.random.normal(average, variance, sample_size)
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Paramètres de simulation
+    """)
+    return
+
+
 @app.cell
 def _():
-    tested_arrivals_per_minutes = [10, 67, 100] # Taux d'arrivés testés
-    MU_Q = 280.56                               # Moyenne de temps de jeux d'un joueur
-    SIGMA_Q = 50.38                             # Variance du temps de jeux
-    sample_size = 10000                         # N
+    tested_arrivals_per_minutes = [10, 67, 100] # Taux d'arrivés testés (branchements / minutes)
+    MU_Q = 280.56                               # Moyenne de temps de jeux d'un joueur (minutes)
+    SIGMA_Q = 50.38                             # Variance du temps de jeux (minutes)
+    sample_size = 10000                         # N (minutes)
     return MU_Q, SIGMA_Q, sample_size, tested_arrivals_per_minutes
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Fonction de simulation
+    Avec le taux de branchements, la transformation inverse calculé plus haut est utilisé pour
+    donnée un nombre de nouveau joueurs à chaque minutes de la simulation.
+    Leurs temps d'arrivé est calculé progressivement au fur et à mesure que le temps avance.
+    Par la suite, un nombre de temps de jeux est généré pour chaque branchements.
+    Le temps de départ du joueur est ensuite calculé.
+
+    Le nombre de joueurs actif à n'importe quel instant $t$ est finalement calculé en appliquant
+    $$
+        T_{\text{arrivé}} \le t \le T_{\text{départ}}
+    $$
+    """)
+    return
 
 
 @app.cell
@@ -76,6 +237,19 @@ def _(MU_Q, SIGMA_Q):
         return arrival_times, play_time, t_values, active_players
 
     return (simulate,)
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Roulement de la simulation
+    Le code suivant éxecute la simulation monte-carlo pour chaque taux de branchements voulu.
+    Elle vient sauvegarder certaines valeurs analysé afin d'être utilisé dans les histogrammes
+    et des affichages.
+          
+    Elle prouve aussi la réponse au besoin d'être capable de dire combiens de joueurs sont connecté
+    à un instant $t$, et fait une comparaison de la moyenne théorique ($\lambda\times\mu_Q$) versus réel. 
+    """)
+    return
 
 
 @app.cell
@@ -144,12 +318,19 @@ def _(MU_Q, results):
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    # Conclusion de la simulation
-
-    Nous avons simulé différents taux d'arrivée de joueurs et observé:
-    - Comment la moyenne réelle se rapproche ou diverge de la moyenne théorique.
-    - Les pics de joueurs simultanés.
-    - Les distributions des arrivées et des temps de jeu.
+    ## Conclusion de la simulation
+    C'est une belle simulation, mais malheureusement elle est très loin d'une situation réel.
+    En effet, elle assume un taux de branchement constant à travers la journée, tandis que le taux change en fonction de l'heure.
+    Elle assume aussi une moyenne de temps de jeux constant, alors qu'en réalité, les gens jouent plus les fins de semaines que les journées de travail.
+          
+    Tout ceci ne prend pas en compte le plus gros problème potentiel. Lorsqu'un jeux est mis sur le marché, on observe une fonction décroissante du
+    nombre de joueur à travers le temps, avec un énorme montant de joueurs au tout début de la mise en marché.
+    S'ils utilisent des moyennes pour quantifié leurs besoins en serveurs, la réalité vas vite les rattrapés lors du lancement du jeux.
+          
+    Ils ont besoin au minimum du double de l'infrastructure prévus pour les premières semaines de la mises en marché, avec possibilités d'aggrendissements
+    temporaires lors d'évènements saisoniers ou des periodes de fêtes nationaux.
+          
+    En bref, c'est une bonne simulation de moyenne, mais ça ne devrait pas être la seule métrique de prise de décisions pour l'infrastructure de serveurs.
     """)
     return
 
